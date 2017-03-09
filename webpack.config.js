2853bb7c6c7807d53e134e4c859e615fac892ae2
pehu71 <pehu@pehu.info>
2017-03-09 13:54:10 +0100

    prepared for webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const ngToolsWebpack = require('@ngtools/webpack');
+var webpack = require('webpack');
+
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    entry: './app/boot.ts',
+    output: {
+        path: './dist',
+        publicPath: 'dist/',
+        filename: 'app.main.js'
+    },
+    plugins: [
+        new ngToolsWebpack.AotPlugin({
+            tsConfigPath: './tsconfig.json'
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: true
+        })
+    ],
+    module: {
+        loaders: [
+            { test: /\.scss$/, loaders: ['raw-loader', 'sass-loader'] },
+            { test: /\.css$/, loader: 'raw-loader' },
+            { test: /\.html$/, loader: 'raw-loader' },
+            { test: /\.ts$/, loader: '@ngtools/webpack' }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    }
+};
